73b45cd6940689c2d9e67ec5305acb9e9d7d9b63
User <user985365125745@gmail.com>
2014-12-08 19:13:17 +0100

    test1

--- Test/src/TestA.java
+++ Test/src/TestA.java
@@ -1,4 +1,18 @@
 
 public class TestA {
 
+	private String text;
+
+	public TestA(String text) {
+		this.text = text;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+
 }
